85eb249ca704ad6feb6b56c27649564357f16520
roguey24 <robert.koebke@gmail.com>
2017-10-14 22:36:14 -0400

    first commit

---
+++ js/quotes.js
@@ -0,0 +1,45 @@
+// Array of marketing related quotes, held in the variable "quotes"
+var quotes = [
+  {
+    quote: 'Content marketing is the only marketing left.',
+    source: 'Seth Godin: ',
+    title: 'Best-Selling Author, Entrepreneur, & Marketer | ',
+    blog: '<a href="https://sethgodin.com/blog">Read Blog</a>'
+  },
+  {
+    quote: 'People don\'t buy what you do, they buy why you do it.',
+    source: 'Simon Sinek: ',
+    title: 'Best-Selling Author, Speaker, & Marketing Consultant | ',
+    blog: '<a href="https://startwithwhy.com/media/">Watch Videos</a>'
+  },
+  {
+    quote: 'Content is king, but distribution is queen and she wears the pants.',
+    source: 'Jonathan Perelman: ',
+    title: 'Head of Digital @ ICM Partners | ',
+    blog: '<a href="http://99u.com/videos/23015/jonathan-perelman-content-is-king-distribution-is-queen">Watch 99u Talk</a>'
+  },
+  {
+    quote: 'Content builds relationships, relationships build trust, and trust drives revenue.',
+    source: 'Jay Baer: ',
+    title: 'Internet Pioneer, Entrepreneur, & Best-Selling Author | ',
+    blog: '<a href="http://www.jaybaer.com/blog/">Read Blog</a>'
+  },
+  {
+    quote: 'What really decides consumers to buy or not to buy is the content of your advertising, not its form.',
+    source: 'David Ogilvy: ',
+    title: 'World Famous Copywriter & Ad Man | ',
+    blog: '<a href="http://www.ogilvy.com/about-david-ogilvy/">Learn About Mr. Ogilvy</a>'
+  },
+  {
+    quote: 'The aim of marketing is to make selling superfluous.',
+    source: 'Peter Drucker: ',
+    title: '"The Man Who Invented Management" | ',
+    blog: '<a href="http://www.drucker.institute/about-peter-f-drucker/">Learn About Mr. Drucker</a>'
+  },
+  {
+    quote: 'If you have more money than brains you should focus on outbound marketing. If you have more brains than money, you should focus on inbound marketing.',
+    source: 'Guy Kawasaki: ',
+    title: 'Marketing Specialist & Venture Capitalist | ',
+    blog: '<a href="https://guykawasaki.com/blog/">Read Blog</a>'
+  }
+];
---
+++ js/script.js
@@ -0,0 +1,58 @@
+// event listener to display a random quote on page load
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.addEventListener('load', printQuote, true);
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+// variable set to an empty array to be used later
+var viewedQuotes = [];
+
+// Changes quote every X seconds
+var intervalID = window.setInterval(printQuote, 10000);
+
+// 1. if the quotes array is empty
+// 2. the quotes array is set to equal the viewedQuotes array
+// 3. then the viewedQuotes array is set to an empty array
+// 4. and the getRandomQuote function is called to start the loop over, otherwise,
+// 5. a random index is generated from the quotes array, removed using the splice method, and returned
+// 6. the returned element is stored in the variable random quote and added to the variable viewedQuotes using the push method
+// 7. returned element is logged in the console and again returned
+function getRandomQuote() {
+  if (quotes.length === 0) {
+    quotes = viewedQuotes;
+    viewedQuotes = [];
+    return getRandomQuote();
+  } else {
+      var randomQuote = quotes.splice(Math.floor(Math.random() * quotes.length), 1)[0];
+      viewedQuotes.push(randomQuote);
+      console.log(randomQuote);
+      return randomQuote;
+  }
+}
+
+// 1. calls the getRandomQuote function and stores it in a variable named printQuote
+// 2. create a variable named HTML and concatenate the string into a message to display in the HTML
+// 3. use array.property to access the specific element in the array
+function printQuote() {
+  var printQuote = getRandomQuote();
+  var HTML = '';
+  HTML += '<p class="quote">' + printQuote.quote + '</p>';
+  HTML += '<p class="source">' + printQuote.source;
+  HTML += '<span class="title">' + printQuote.title + '</span>';
+  HTML += '<span class="blog">' + printQuote.blog + '</span>';
+  HTML += '</p>';
+
+  // string is made, which links to the quote-box and dynamically inserts into the HTML
+  // calls the getRandomColor function which generates a random color and then changes the background color of the body
+  // both are included in the printQuote function so they can be triggered in the event listeners
+  document.getElementById('quote-box').innerHTML = HTML;
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+// 1. generates a random color by concantenating the string and number components that make up an rgb color model
+// 2. saves it to a variable named color
+// 3. returns the color variable
+function getRandomColor () {
+  var color = 'rgb(' + Math.floor(Math.random()*200) + ', ' + Math.floor(Math.random()*200) + ', ' + Math.floor(Math.random()*200) + ')';
+  return color;
+}
